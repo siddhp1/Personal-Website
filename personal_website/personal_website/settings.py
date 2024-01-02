22cd515b7c300ff0e2a05472ff82148243bfbb42
Siddh Patel <47033295+siddhp123@users.noreply.github.com>
2024-01-02 17:05:17 -0500

    set debug to false

--- personal_website/personal_website/settings.py
+++ personal_website/personal_website/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -23,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%jhez+k@phu^@uq&qy(l@70=iqb3785(%l!4hkzh)mhk@oq)+9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
